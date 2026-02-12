0844c7fbc77235bbb02fd764029df69b53331698
gnanaraj0898 <gnanaraj0809@gmail.com>
2026-02-12 23:13:30 +0530

    added perfect pod yaml, sample pod yaml, resource tag lambda, python practices code.

--- Terraform_EKS/EC2-only_cluster/python/new/stringrotation.py
+++ Terraform_EKS/EC2-only_cluster/python/new/stringrotation.py
@@ -4,5 +4,5 @@ def rotate(s: str, k: int):
     rotated = s[k_mod:] + s[:k_mod]
     return rotated
 
-rotated_key = rotate("Highlight@123", 4) #takes the first four letter of the string and put(rotate) it in the last
+rotated_key = rotate("Highlight@123", 14) #takes the first four letter of the string and put(rotate) it in the last
 print(f"rotated string is {rotated_key}")
